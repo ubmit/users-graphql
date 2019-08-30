970193ddbcc2ef0d7ffe430f09b6ade814e79e89
Guilherme de Andrade <guilhermedeandrade@me.com>
2019-08-30 10:48:31 +0100

    first example from Stephen Grider's course

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql");
+const schema = require("./schema/schema");
+
+const PORT = 4000;
+
+const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`listening on https://localhost:${PORT}`);
+});
